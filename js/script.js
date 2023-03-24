633cd5a757030c424b11eac4a8520520306fc6e0
Nazwan Hilmi <nhr.bzl@gmail.com>
2023-03-24 16:40:42 +0700

    push

---
+++ js/script.js
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+// Owlcarousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    center: true,
+    navText: [
+	    "<i class='fa fa-angle-left'></i>",
+	    "<i class='fa fa-angle-right'></i>"
+	],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
+=======
+// Owlcarousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    center: true,
+    navText: [
+	    "<i class='fa fa-angle-left'></i>",
+	    "<i class='fa fa-angle-right'></i>"
+	],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
+>>>>>>> a71e6ee88d77c148def1fa4ee4391ccc22822d92
+});
---
+++ script.js
@@ -0,0 +1,23 @@
+// Hamburger
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector('.menu-nav');
+
+menuBar.addEventListener("click", function(){
+    menuNav.classList.toggle("menu-active");
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navigation');
+    const scrolled = window.scrollY > navbar.offsetTop;
+
+    if (scrolled) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+         $(this).ekkoLightbox({alwaysShowClose: true});
+});
